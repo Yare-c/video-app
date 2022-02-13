fae1ba2d3291a5156e1b61c1b5e949d954161491
Yare-c <74290193+Yare-c@users.noreply.github.com>
2022-02-13 12:13:02 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import time
+import os
+
+
+seconds = time.time()
+local_time = time.ctime(seconds)
+
+path = "/Users/yaroslavkorolev/Documents/films/2.mp4"
+
+def start_video():
+    os.open('r ')
+    time.sleep(5)
+    os.end(path)
+
+start_video()
+
+
+time.sleep(10)
+start_video()
